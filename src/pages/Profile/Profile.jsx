d4821ecbf070bff4e170e75ff16379a3ae090243
Ubaton <raymondNG729@gmail.com>
2023-12-08 22:01:03 +0200

    v2.0.5

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,8 +2,26 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <div className="">
+    <div className="text-gray-50">
       <h1 className="text-center text-2xl p-4">Profile</h1>
+      <p className="text-center px-6 pb-2">
+        I am a highly skilled Full-stack developer with expertise in web and
+        mobile application development. With a strong command of front-end and
+        back-end technologies, I create innovative and user-friendly solutions.
+        Let's build exceptional applications together!
+      </p>
+
+      <p className="text-center px-6 pb-2">
+        Experienced Full-stack Software Developer with 4+ years of expertise.
+        Specialized in building robust web applications, proficient in front-end
+        (HTML, CSS, JavaScript, React, React Native, Next JS, Angular) and
+        back-end (Python, Node.js, Django, Express.js) development. Strong
+        database management skills (SQL, NoSQL). Committed to delivering
+        high-quality code and user-friendly interfaces. Skilled in
+        problem-solving and collaborative team environments. Adaptable to new
+        technologies, providing end-to-end development solutions tailored to
+        client needs.
+      </p>
     </div>
   );
 };
