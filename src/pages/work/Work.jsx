0bdc03d8804cbb7ad489f61d45ba6f71789827d2
Ubaton <raymondNG729@gmail.com>
2023-12-20 13:50:28 +0200

    v2.1.13

--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -3,9 +3,26 @@ import { useRouter } from "next/navigation";
 
 const WorkPage = () => {
   return (
-    <div>
-      <Profile />
-    </div>
+    <>
+      <div className="bg-zinc-500/30 backdrop-blur-sm">
+        <Profile />
+      </div>
+      <div className="min-h-screen overflow-hidden">
+        <div className="container mx-auto">
+          <h1 className="text-2xl font-bold text-center pt-12">My Work</h1>
+          <div className="flex flex-col p-4 space-y-4">
+            <di className="flex flex-row">
+              <div className="w-[20rem] h-[22rem] bg-zinc-500 rounded-l-xl"></div>
+              <div className="w-[42rem] h-[22rem] bg-zinc-500 rounded-r-xl"></div>
+            </di>
+            <di className="flex flex-row">
+              <div className="w-[42rem] h-[22rem] bg-zinc-500 rounded-l-xl"></div>
+              <div className="w-[20rem] h-[22rem] bg-zinc-500 rounded-r-xl"></div>
+            </di>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
