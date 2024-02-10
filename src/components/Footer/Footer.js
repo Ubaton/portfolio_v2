e5af61543dd6991a8a9f83c8a9e99d9872736894
Ubaton <raymondNG729@gmail.com>
2024-02-10 10:44:15 +0200

    v0.0.4 Add footer and Profile Link to PDF Resume.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Layout from "../Layout";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="w-full border-t-2 border-solid border-dark font-medium text-lg">
+        <Layout className="flex items-center justify-between py-8 ">
+          <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
+          <div className="flex items-center">
+            Build with{" "}
+            <span className="text-primary text-2xl px-1">&#9825;</span>
+            by&nbsp;
+            <Link
+              href="/Raymond"
+              target={"_blank"}
+              className="underline underline-offset-2"
+            >
+              Raymond Ngobeni
+            </Link>
+          </div>
+          <Link
+            href="/Hello"
+            target={"_blank"}
+            className="underline underline-offset-2"
+          >
+            Say Hello
+          </Link>
+        </Layout>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/HireMe/HireMe.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { CircularText } from "../icons/icons";
+import Link from "next/link";
+
+const HireMe = () => {
+  return (
+    <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden ">
+      <div className="flex items-center justify-center relative w-48 h-auto">
+        <CircularText className={"full-dark animate-spin-slow"} />
+
+        <Link
+          href="mailto:mrrngobeni6@gamil.com"
+          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light hover:bg-light hover:text-dark shadow-md border border-solid border-dark font-semibold w-24 h-24 rounded-full"
+        >
+          Hire Me
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HireMe;
